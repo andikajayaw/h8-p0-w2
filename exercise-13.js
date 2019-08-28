3a4f17162180223fe8f064f91f3dd682dda0875d
andikajayaw <andikajayawiguna@gmail.com>
2019-08-28 16:55:25 +0700

    add exercise-13.js

---
+++ exercise-13.js
@@ -0,0 +1,39 @@
+function xo(str) {
+    // you can only write your code here!
+    var x = 0;
+    var o = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === 'x') {
+            x++
+        } else if (str[i] === 'o') {
+            o++
+        }
+    }
+    if (x === o) {
+        return true;
+    } else {
+        return false;
+    }
+    // for (var i = 0; i < str.length; i++) {
+    //     if (str[i] === 'x') {
+    //         x += [i]
+    //     } else if (str[i] === 'o') {
+    //         o += [i]
+    //     }
+    // }
+
+    // if (x.length === o.length) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+}
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
